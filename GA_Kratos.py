a16cf4852a1c32272fe2393c0a444ce94b6caf80
Chengshun Shang <chengshun.shang1996@gmail.com>
2023-03-17 12:07:52 +0100

    minor

--- GA_Kratos.py
+++ GA_Kratos.py
@@ -453,7 +453,7 @@ class GA:
  
  
 if __name__ == "__main__":
-    CXPB, MUTPB, NGEN, popsize = 0.8, 0.1, 1000, 45  # popsize must be even number
+    CXPB, MUTPB, NGEN, popsize = 0.8, 0.1, 1000, 46  # popsize must be even number
     aim_strength, aim_young_modulus = 4.323e7, 5.54e9
  
     up = [1e8, 1e8, 1e10, 1e10]  # upper range for variables
