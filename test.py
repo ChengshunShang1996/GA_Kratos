3ce9b17eb799b274494fa2c8aa5d050d89a6aab2
Chengshun Shang <chengshun.shang1996@gmail.com>
2023-04-03 18:34:44 +0200

    update for ML

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
 import csv  
 
-header = ['name', 'area', 'country_code2', 'country_code3']
-data = ['Afghanistan', 652090, 'AF', 'AFG']
-my_data = []
+A = 10 / 3
 
-with open('countries.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-
-    # write the header
-    writer.writerow(header)
-    my_data.append(data[0])
-    my_data.append(data[1])
-    my_data.append(data[2])
-    # write the data
-    writer.writerow(my_data)
+print(A)
